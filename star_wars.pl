093bef85b1830c215bd665768e1927416cd9b95d
Anna Alicia Vasconcelos Milani <aavm.snf19@uea.edu.br>
2021-05-26 20:38:11 -0400

    exercicio star wars finalizando

--- star_wars.pl
+++ star_wars.pl
@@ -1,13 +1,16 @@
-homem(vader).
-homem(luke).
-homem(sidious).
-mulher(leia).
-homem(obiwan).
+jedi(anakin).
+jedi(luke).
+jedi(leia).
+jedi(yoda).
+jedi(obiwan).
+jedi(quigon).
 
 sith(vader).
 sith(sidious).
 sith(maul).
 sith(tarkin).
+
+virou_sith(anakin).
 
 pai(vader, luke).
 pai(vader, leia).
@@ -16,17 +19,12 @@ filho(X, Y) :- pai(Y, X).
 irmao(X, Y) :- pai(P, X), pai(P, Y), X\=Y.
 
 
-mestre(yoda, luke).
-mestre(sidious, vader).
-
 mestrando(luke, yoda).
 mestrando(vader, sidious).
 
-
-%mestre(X, Y) :- mestrando(Y, X).
+mestre(X, Y) :- mestrando(Y, X).
 
 matou(vader, obiwan).
-matou(maul, obiwan).
 matou(luke, tarkin).
 matou(luke, jabba).
 matou(vader, sidous).
